8e7a1609cd909c18f3b4be7cc487bec777754860
Shiny Kiwis <nguyenhua.hcm@gmail.com>
2022-12-03 18:03:01 +0700

    Update: Implement checkbox for sidebar categories section

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,55 +1,73 @@
-import {useState} from 'react'
-import "./Sidebar.css"
+import { useState } from "react";
+import "./Sidebar.css";
 
-const CategoryItem = ({selected, title,handleSelect}) => {
+const CategoryItem = ({ isSelected, title, setSelected, selected }) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheck = () => {
+    setIsChecked(!isChecked);
+    if (!selected.includes(title)) {
+      setSelected((selected) => [...selected, title]);
+    } else {
+      setSelected((selected) =>
+        selected.filter((selectedItem) => selectedItem !== title)
+      );
+    }
+  };
   return (
-    <label className={`${selected ? "sidebar_item active": "sidebar_item"}`}
-    onClick={(e) => handleSelect(e, title)}>{title}      
-      <input type="checkbox"/>
-      <span className='checkmark'></span>
+    <label
+      className="sidebar_item"
+      style={isSelected ? { color: "#638aa7", fontWeight: "bold" } : {}}
+    >
+      {title}
+      <input type="checkbox" checked={isChecked} onChange={handleCheck} />
+      <span className="checkmark"></span>
     </label>
-  )
-}
+  );
+};
 
 const Sidebar = () => {
-  const [selected, setSelected] = useState([])
-  const categories = ["Toys", "Eletronics", "Clothes", "Housewares", "LifeStyles", "PC&Laptop", "Watch","Health","Sport&Traveling","Bookstore" ]
-  const handleSelect = (e, title) => {
-    e.preventDefault()
-    if(!selected.includes(title)) {
-      setSelected(selected => [...selected, title])
-    } else {
-      setSelected(selected => selected.filter(selectedItem => selectedItem != title))
-    }
-  }
-  console.log(selected)
+  const [selected, setSelected] = useState([]);
+  const categories = [
+    "Toys",
+    "Eletronics",
+    "Clothes",
+    "Housewares",
+    "LifeStyles",
+    "PC&Laptop",
+    "Watch",
+    "Health",
+    "Sport&Traveling",
+    "Bookstore",
+  ];
+
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <h3>Price</h3>
-      <div className='options'>
+      <div className="options">
         <button>Price: From low to high</button>
         <button>Price: From high to low</button>
       </div>
       <h3>Category</h3>
-        <div className='options'>
-          <div className='category_options'>
-            {categories.map( category => (
-              <CategoryItem 
-                selected={selected.includes(category)}
-                title={category} 
-                handleSelect={handleSelect} 
-                key={category} 
-              />
-            ))}
-          </div>
+      <div className="options">
+        <div className="category_options">
+          {categories.map((category) => (
+            <CategoryItem
+              isSelected={selected.includes(category)}
+              title={category}
+              selected={selected}
+              setSelected={setSelected}
+              key={category}
+            />
+          ))}
         </div>
+      </div>
       <h3>Stock Availability</h3>
-      <div className='options'>
+      <div className="options">
         <button>Stock: In stock</button>
         <button>Stock: Out of stock</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
